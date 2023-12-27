5beaafd537b6de7023873eb28e5b2bdd0a43545c
hana0229 <gksk5114@naver.com>
2023-12-27 13:06:15 +0900

    두번째 메뉴별그림

---
+++ script/script01.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".main>li, .img_area").mouseenter(function(){
+        $(".sub, .img_area").stop().show();
+    })//mouseenter
+    $(".main>li, .img_area").mouseleave(function(){
+
+    })//mouseleave
+})//jQuery
